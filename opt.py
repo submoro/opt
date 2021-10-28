539d33c9f6a1bd541e75fbe0249afecaaf8a1ee5
submoro <40717644+submoro@users.noreply.github.com>
2021-10-28 16:56:17 +0300

    Update opt.py

--- opt.py
+++ opt.py
@@ -147,10 +147,21 @@ pe_min = int(df['trailingPE'].min())
 pe_max = int(df['trailingPE'].max())
 po_value = st.number_input('Enter Required Portfolio Value in "SAR" - Min = 10 SAR, Max = 1,000,000 SAR:', min_value = 10, max_value = 1000000,step = 100)
 port_value = st.slider('Portfolio Value (SAR):', 1000, 1000000,po_value,step = 100)
+
+st.markdown('The price-to-earnings ratio (P/E) is one of the most widely used tools by which investors and analysts determine a stocks relative valuation.')
+st.markdown('The P/E ratio helps one determine whether a stock is overvalued or undervalued. A company P/E can also be benchmarked against other stocks in the same industry or against the broader market')
 price_to_earning = st.slider('Maximum Price To earning:', pe_min, pe_max,20)
+
+st.markdown('The dividend yield, expressed as a percentage, is a financial ratio (dividend/price) that shows how much a company pays out in dividends each year relative to its stock price.')
 min_div = st.slider('Minimum Div (%):', int(df['trailingAnnualDividendYield'].min()*100), int(df['trailingAnnualDividendYield'].max()*100),4)
+
+st.markdown('The payout ratio is a financial metric showing the proportion of earnings a company pays its shareholders in the form of dividends, expressed as a percentage of the company total earnings.')
 max_pay_ratio = st.slider('Maximum Payout Ratio (%):', int(df['payoutRatio'].min()*100), int(df['payoutRatio'].max()*100),75)
+
+st.markdown('The price-to-book ratio compares a company market value to its book value. The market value of a company is its share price multiplied by the number of outstanding shares. The book value is the net assets of a company')
 price_to_book = st.slider('Maximum Price to Book (Multiple):', int(df['priceToBook'].min()), int(df['priceToBook'].max()),3)
+
+st.markdown('The enterprise-value-to-EBITDA ratio is calculated by dividing EV by EBITDA or earnings before interest, taxes, depreciation, and amortization. Typically, EV/EBITDA values below 10 are seen as healthy')
 ev_to_ebtida = st.slider('Maximum Enterprice to EBTIDA:', int(df['enterpriseToEbitda'].min()), int(df['enterpriseToEbitda'].max()),25)
 
 opt(port_value,price_to_earning,min_div/100,max_pay_ratio/100,price_to_book,ev_to_ebtida)
@@ -178,6 +189,8 @@ cost = [a*b for a,b in zip(price,qty)]
 result = pd.DataFrame({'Name': Name,'Price':price ,'Quantities': qty, 'Div':Divd, 'PE':Pe, 'EV/EB':eveb,'PBV':pbv,'Payout_ratio':payout,'Cost': cost})
 
 st.subheader('Result')
+
+st.markdown('Find below the company name, purchase price, Quantity of each stock to purchase and few other parameters for your information about each stock')
 st.write(result.tail())
 
 
@@ -205,7 +218,9 @@ portfolio = pd.DataFrame({'Ratio': ['Portfolio Value','Price to Earning','Enterp
 'Value' : [Portfolio_Value,Portfolio_PE,Portfolio_EVEB,Portfolio_PBV,Portfolio_Div,Portfolio_payout]})
 
 st.subheader('Portfolio Figures')
+st.markdown('Below is your portfolio performance if you purchase these stocks with the mentioned prices and quantities')
 st.write(portfolio.tail())
 
 st.subheader('UnderValued Stocks')
+st.markdown('Below you can find all stock list which we believe are undervalued compairing their current Dividend yeild to its 5 years average dividend yeild')
 st.write(dx.tail())
