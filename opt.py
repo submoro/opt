856938048607a9947a6a921766240ea741ad1add
submoro <40717644+submoro@users.noreply.github.com>
2021-11-01 14:45:20 +0300

    Update opt.py

--- opt.py
+++ opt.py
@@ -59,7 +59,7 @@ dx.sort_values('DivCom', ascending = False)
 #-------------------------------------------------------------------
 @st.cache(allow_output_mutation=True, ttl = 86400)
 def data_prep(data,flag):
-  cols = ['trailingAnnualDividendYield','trailingPE','returnOnEquity','priceToBook','enterpriseToEbitda','fiveYearAvgDividendYield','payoutRatio'.'debtToEquity']
+  cols = ['trailingAnnualDividendYield','trailingPE','returnOnEquity','priceToBook','enterpriseToEbitda','fiveYearAvgDividendYield','payoutRatio','debtToEquity']
   for col in cols:
     data[col] = data[col].astype(float)
 
